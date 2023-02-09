35c1ed25ee6955f9308b883d6575c3e431809746
Jess <jessicadixie14@hotmail.com>
2023-02-08 18:16:40 -0800

    Set up JS, CSS, and HTML files. Added logic to add books into library.

---
+++ app.js
@@ -0,0 +1,83 @@
+const libraryDiv = document.querySelector('.library');
+const newBookBtn = document.querySelector('.newBookBtn');
+
+const submitNewBookForm = document.getElementById('submitNewBookForm');
+const newBookSubmit = document.querySelector('#newBookSubmit');
+
+const library = [];
+let newBook;
+
+class Book {
+    constructor(title, author, numPages, haveRead) {
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+        this.haveRead = haveRead;
+    }
+}
+
+newBookBtn.addEventListener("click", e => {
+    e.preventDefault();
+    submitNewBookForm.classList.toggle('hidden');
+})
+
+newBookSubmit.addEventListener("click", addBookToLibrary);
+
+function addBookToLibrary(e) {
+    e.preventDefault();
+    if(submitNewBookForm.checkValidity()) {
+        newBook = new Book(title, author, numPages, haveRead);
+        library.push(newBook);
+        renderBooks();
+    } else {
+        return false;
+    }
+}
+
+function renderBooks() {
+    // Clear books first to avoid duplicates
+    while(libraryDiv.firstChild) {
+        libraryDiv.removeChild(libraryDiv.lastChild);
+    }
+    for(let i = 0; i < library.length; i++) {
+        createBook(library[i]);
+    }
+}
+
+
+function createBook(i) {
+    const bookDiv = document.createElement('section');
+    const titleDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const numPagesDiv = document.createElement('div');
+    const haveReadDiv = document.createElement('div');
+    const removeBookBtn = document.createElement('span');
+
+    bookDiv.classList.add('book');
+    bookDiv.setAttribute('data-book_id', library.indexOf(i));
+
+    titleDiv.classList.add('title');
+    titleDiv.textContent = i.title.value;
+    bookDiv.appendChild(titleDiv);
+
+    authorDiv.classList.add('author');
+    authorDiv.textContent = i.author.value;
+    bookDiv.appendChild(authorDiv);
+
+    numPagesDiv.classList.add('numPages');
+    numPagesDiv.textContent = i.numPages.value;
+    bookDiv.appendChild(numPagesDiv);
+
+    haveReadDiv.classList.add('haveRead');
+    bookDiv.appendChild(haveReadDiv);
+    if(!i.haveRead.checked) {
+        haveReadDiv.textContent = 'Not read';
+    } else {
+        haveReadDiv.textContent = 'Read';
+    }
+
+    removeBookBtn.classList.add('.removeBookBtn');
+    bookDiv.appendChild(removeBookBtn);
+
+    libraryDiv.appendChild(bookDiv);
+}
