52647de098ac26765201a2e1da983609c83e0f8a
Jess <jessicadixie14@hotmail.com>
2023-02-16 20:43:49 -0800

    Changed styling to inline look, fixed createBook() logic, added basics of the Have Read toggler

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const library = [];
 let newBook;
 
 const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-btn');
 
 class Book {
     constructor(title, author, numPages, haveRead) {
@@ -23,6 +24,7 @@ newBookBtn.addEventListener("click", e => {
     e.preventDefault();
     form.classList.toggle('hidden');
     overlay.classList.toggle('hidden');
+    overlay.classList.toggle('active');
 })
 
 newBookSubmit.addEventListener("click", addBookToLibrary);
@@ -30,6 +32,10 @@ newBookSubmit.addEventListener("click", addBookToLibrary);
 function addBookToLibrary(e) {
     e.preventDefault();
     if(submitNewBookForm.checkValidity()) {
+        const title = document.getElementById('title').value;
+        const author = document.getElementById('author').value;
+        const numPages = document.getElementById('numPages').value;
+        const haveRead = document.getElementById('haveRead').checked;
         newBook = new Book(title, author, numPages, haveRead);
         library.push(newBook);
         renderBooks();
@@ -48,40 +54,70 @@ function renderBooks() {
     }
 }
 
+const toggleButtons = document.querySelectorAll('.switch input[type="checkbox"]');
+    toggleButtons.forEach(button => {
+        button.addEventListener('change', (e) => {
+            const bookId = e.target.closest('.book').getAttribute('data-book_id');
+            library[bookId].haveRead = e.target.checked;
+        });
+    });
 
-function createBook(i) {
+function createBook(book) {
     const bookDiv = document.createElement('section');
     const titleDiv = document.createElement('div');
     const authorDiv = document.createElement('div');
     const numPagesDiv = document.createElement('div');
     const haveReadDiv = document.createElement('div');
-    const removeBookBtn = document.createElement('span');
+    const readToggleLabel = document.createElement('label');
+    var readToggleInput = document.createElement('input');
+    const readToggleSlider = document.createElement('span');
+    const removeBookBtn = document.createElement('button');
 
     bookDiv.classList.add('book');
-    bookDiv.setAttribute('data-book_id', library.indexOf(i));
+    bookDiv.setAttribute('data-book_id', library.indexOf(book));
 
     titleDiv.classList.add('title');
-    titleDiv.textContent = i.title.value;
+    titleDiv.textContent = book.title;
     bookDiv.appendChild(titleDiv);
 
     authorDiv.classList.add('author');
-    authorDiv.textContent = i.author.value;
+    authorDiv.textContent = book.author;
     bookDiv.appendChild(authorDiv);
 
-    numPagesDiv.classList.add('numPages');
-    numPagesDiv.textContent = i.numPages.value;
+    numPagesDiv.textContent = book.numPages + (book.numPages == 1 ? ' page' : ' pages');
     bookDiv.appendChild(numPagesDiv);
 
     haveReadDiv.classList.add('haveRead');
     bookDiv.appendChild(haveReadDiv);
-    if(!i.haveRead.checked) {
-        haveReadDiv.textContent = 'Not read';
-    } else {
-        haveReadDiv.textContent = 'Read';
-    }
+    haveReadDiv.textContent = book.haveRead == true ? 'Read' : 'Not read';
 
-    removeBookBtn.classList.add('.removeBookBtn');
+    readToggleLabel.classList.add('switch');
+    readToggleLabel.appendChild(readToggleInput);
+    readToggleLabel.appendChild(readToggleSlider);
+    readToggleSlider.classList.add('slider');
+
+    readToggleInput.type = 'checkbox';
+    readToggleInput.checked = book.haveRead;
+    readToggleInput.addEventListener('change', e => {
+        book.haveRead = e.target.checked;
+        haveReadDiv.textContent = book.haveRead == true ? 'Read' : 'Not read';
+    })
+    haveReadDiv.appendChild(readToggleLabel);
+
+    removeBookBtn.classList.add('removeBookBtn');
+    removeBookBtn.textContent = 'Delete';
     bookDiv.appendChild(removeBookBtn);
 
     libraryDiv.appendChild(bookDiv);
+}
+
+closeBtn.addEventListener("click", closeForm);
+overlay.addEventListener("click", closeForm);
+
+function closeForm(e) {
+    e.preventDefault();
+    overlay.classList.add('hidden');
+    overlay.classList.remove('active');
+    form.classList.add('hidden');
+    form.classList.remove('active');
 }
